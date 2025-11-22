ca4d4b808d5ebf2dda1fbcaf55b4921244cc8c9f
CluLess5375 <alexander.brands@bitprojects.be>
2025-11-22 12:03:47 +0100

    Add dynamic logbook entry count to dashboard
    Introduced a new function in script.js to count logbook entries for Clement and Alexander using their respective IDs and update the dashboard total dynamically. Updated index.html to assign unique IDs to logbook entry elements for both users and the total entry count span.

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-let urenAlexander = 12;
-let urenClement = 18;
+    let urenAlexander = 12;
+    let urenClement = 18;
 
-function laadPagina() {
-    totaalUren();
-}
+    function laadPagina() {
+        totaalUren();
+        logboekEntries();
+    }
 
-function totaalUren() {
-    urenAlexander = parseInt(document.getElementById("urenAlexander").textContent);
-    urenClement = parseInt(document.getElementById("urenClement").textContent);
+    function totaalUren() {
+        urenAlexander = parseInt(document.getElementById("urenAlexander").textContent);
+        urenClement = parseInt(document.getElementById("urenClement").textContent);
 
-    let totaal = urenAlexander + urenClement;
+        let totaal = urenAlexander + urenClement;
 
-    document.getElementById("totaleUren-Dashboard").innerText = totaal + "u";
-    document.getElementById("totaleUren-Dashboard-Card").innerText = totaal + "u";
-    console.log("Totaal aantal uren: " + totaal);
-}
+        document.getElementById("totaleUren-Dashboard").innerText = totaal + "u";
+        document.getElementById("totaleUren-Dashboard-Card").innerText = totaal + "u";
+        console.log("Totaal aantal uren: " + totaal);
+    }
+
+    function logboekEntries()
+    {
+        let entriesClement = document.querySelectorAll("#logboek-item-clement");
+        let itemsClement = 0;
+        let totaalAantalEntries = 0;
+
+        entriesClement.forEach(() => {
+            itemsClement++;
+        });
+
+        totaalAantalEntries = itemsClement;
+
+        let entriesAlexander = document.querySelectorAll("#logboek-item-alexander");
+        let itemsAlexander = 0;
+        entriesAlexander.forEach(() => {
+            itemsAlexander++;
+        });
+
+        totaalAantalEntries += itemsAlexander;
+
+
+        document.getElementById("totaal-Aantal-Entries").innerText = totaalAantalEntries;
+    }
